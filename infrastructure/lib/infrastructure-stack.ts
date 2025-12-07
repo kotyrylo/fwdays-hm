8c75d8a7867482ef0b930178ca11d194385ea8a4
kotyrylo <kyrylo.osadchyi@amo.tech>
2025-12-07 22:20:25 +0100

    fwdays hm-3

--- infrastructure/lib/infrastructure-stack.ts
+++ infrastructure/lib/infrastructure-stack.ts
@@ -5,36 +5,6 @@ import * as cloudfront from 'aws-cdk-lib/aws-cloudfront';
 import * as origins from 'aws-cdk-lib/aws-cloudfront-origins';
 import { BucketDeployment, Source} from 'aws-cdk-lib/aws-s3-deployment';
 
-// const websiteBucket = new s3.Bucket(this, 'KyrylosFWDaysHM3', {
-//       removalPolicy: cdk.RemovalPolicy.DESTROY,
-//       autoDeleteObjects: true,
-//     });
-
-//     new cloudfront.Distribution(this, 'KyrylosFWDaysHM3Distribution', {
-//       defaultBehavior: {
-//         origin: new origins.S3Origin(websiteBucket),
-//         viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-//       },
-//       defaultRootObject: 'index.html',
-//       errorResponses: [
-//         {
-//           httpStatus: 403,
-//           responseHttpStatus: 200,
-//           responsePagePath: '/index.html',
-//         },
-//         {
-//           httpStatus: 404,
-//           responseHttpStatus: 200,
-//           responsePagePath: '/index.html',
-//         },
-//       ],
-//     })
-
-//     new BucketDeployment(this, 'KyrylosFWDaysHM3Deployment', {
-//       sources: [Source.asset('../frontend/dist')],
-//       destinationBucket: websiteBucket,
-//     });
-
 export class InfrastructureStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
@@ -46,38 +16,12 @@ export class InfrastructureStack extends cdk.Stack {
         origin: new origins.S3Origin(websiteBucket),
         viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
       },
-      defaultRootObject: 'index.html',
-      errorResponses: [
-        {
-          httpStatus: 403,
-          responseHttpStatus: 200,
-          responsePagePath: '/index.html',
-        },
-        {
-          httpStatus: 404,
-          responseHttpStatus: 200,
-          responsePagePath: '/index.html',
-        },
-      ],
+      defaultRootObject: 'index.html'
     })
 
     new BucketDeployment(this, 'KyrylosFWDaysHM3Deployment', {
       sources: [Source.asset('../frontend/dist')],
       destinationBucket: websiteBucket,
     });
-
-    // const distribution = new cloudfront.Distribution(this, 'KyrylosFWDaysHM3Distribution', {
-    //   defaultBehavior: {
-    //     origin: new origins.S3StaticWebsiteOrigin(websiteBucket),
-    //     viewerProtocolPolicy: cloudfront.ViewerProtocolPolicy.REDIRECT_TO_HTTPS,
-    //   },
-    //   defaultRootObject: 'index.html'
-    // })
-
-    // new BucketDeployment(this, 'KyrylosFWDaysHM3Deployment', {
-    //   sources: [Source.asset('../frontend/dist')],
-    //   destinationBucket: websiteBucket,
-    //   distribution,
-    // });
   }
 }
